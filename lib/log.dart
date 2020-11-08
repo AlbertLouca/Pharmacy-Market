cc1c3efb6e604333b36a6631d6a23d2a4b90e1f9
Farouk00 <44416222+Farouk00@users.noreply.github.com>
2020-11-08 20:37:45 +0200

    Add files via upload

---
+++ lib/log.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Login',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: new LoginPage(),
+    );
+  }
+}
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+
+enum FormType {
+  login,
+  register
+}
+
+
+
+class _LoginPageState extends State<LoginPage> {
+    
+  FormType _form = FormType.login; 
+ 
+ 
+  void _formChange () async {
+    setState(() {
+      if (_form == FormType.register) {
+        _form = FormType.login;
+      } else {
+        _form = FormType.register;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold( bottomNavigationBar: BottomNavigationBar(  
+       currentIndex: 0, // this will be set when a new tab is tapped
+       items: [
+        BottomNavigationBarItem(
+      icon: Icon(Icons.account_circle),
+      label: 'Profile',
+      
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.settings),
+      label: 'Settings',
+    ),
+  ],
+     ),
+      appBar: _buildBar(context),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Column(
+          children: <Widget>[
+            _buildTextFields(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      title: new Text("Register"),
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildTextFields() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            child: new TextField(
+              decoration: new InputDecoration(
+                labelText: 'Email'
+              ),
+               //obscureText: true,
+            ),
+          ),
+          new Container(
+            child: new TextField(
+              decoration: new InputDecoration(
+                labelText: 'Password'
+              ),
+              obscureText: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    if (_form == FormType.login) {
+      return new Container(
+        child: new Column(
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text('Login'),
+              onPressed: _pressedLogin,
+            ),
+            new FlatButton(
+              child: new Text('Dont have an account? Tap here to register.'),
+              onPressed: _formChange,
+            ),
+            new FlatButton(
+              child: new Text('Forgot Password?'),
+              onPressed: _pressedForgot,
+            )
+          ],
+        ),
+      );
+    } else {
+      return new Container(
+        child: new Column(
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text('Create an Account'),
+              onPressed: _createAccountPressed,
+            ),
+            new FlatButton(
+              child: new Text('Have an account? Click here to login.'),
+              onPressed: _formChange,
+            )
+          ],
+        ),
+      );
+    }
+  }
+
+  void _pressedForgot () {
+    print('Forgot Button was pressed');
+  }
+
+  void _createAccountPressed () {
+    print('The user wants to create an account');
+
+  }
+
+void _pressedLogin () {
+    print('Login Button was pressed');
+  }
+
+}
+
+
