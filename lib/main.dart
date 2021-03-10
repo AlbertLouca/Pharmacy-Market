73d7725291baca95721f0ed916a54f69fd33c4cd
AlbertLouca <35044146+AlbertLouca@users.noreply.github.com>
2021-03-10 10:29:17 +0200

    gui

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget{
 
       //initialRoute: MyMaps.id,
 
-      initialRoute: AddProduct.id,
+      initialRoute: CategoryScreen.id,
 
 
      // initialRoute: SignupScreen.id,
